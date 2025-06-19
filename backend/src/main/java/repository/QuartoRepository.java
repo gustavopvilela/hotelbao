a9a8884914a868a49831ec7bf1db229cd20fa04d
gustavopvilela <gustavohp.vilela@gmail.com>
2025-06-19 14:28:45 -0300

    ajuste de pastas

--- backend/src/main/java/repository/QuartoRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import entity.Quarto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuartoRepository extends JpaRepository<Quarto, Long> {
-}

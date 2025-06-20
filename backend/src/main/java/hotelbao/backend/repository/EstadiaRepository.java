9803d962e081f3aed0c9f51c49ff845f6297a8a5
gustavopvilela <gustavohp.vilela@gmail.com>
2025-06-20 17:10:36 -0300

    Opção 8 do menu de admin: limpar banco de dados (ainda sem quartos)

--- backend/src/main/java/hotelbao/backend/repository/EstadiaRepository.java
+++ backend/src/main/java/hotelbao/backend/repository/EstadiaRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EstadiaRepository extends JpaRepository<Estadia, Long> {
+    void deleteAll ();
 }
--- backend/src/main/java/hotelbao/backend/service/DatabaseService.java
+++ backend/src/main/java/hotelbao/backend/service/DatabaseService.java
@@ -1,28 +1,26 @@
 package hotelbao.backend.service;
 
+import hotelbao.backend.repository.EstadiaRepository;
 import hotelbao.backend.repository.PasswordRecoverRepository;
 import hotelbao.backend.repository.RoleRepository;
 import hotelbao.backend.repository.UsuarioRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class DatabaseService {
-    @PersistenceContext
-    private EntityManager entityManager;
-
     @Autowired private UsuarioRepository usuarioRepository;
     @Autowired private RoleRepository roleRepository;
     @Autowired private PasswordRecoverRepository passwordRecoverRepository;
-    /* TODO: quando adicionar os outros repositories, colocar aqui. */
+    @Autowired private EstadiaRepository estadiaRepository;
+    /* TODO: adicionar o de quartos aqui. */
 
     @Transactional
     public void limparBancoDeDados () {
         usuarioRepository.deleteAll();
         roleRepository.deleteAll();
         passwordRecoverRepository.deleteAll();
+        estadiaRepository.deleteAll();
     }
 }
